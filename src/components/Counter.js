7aa39d22a3a1fb5faaac602968a28ff4d0054174
Mohd ZAID KHAN <zaidk9620@gmail.com>
2023-08-19 18:03:25 +0530

    using redux data in react components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,13 @@
+import { useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const toggleCounterHandler = () => {};
-
+  const counter=useSelector(stat=>stat.counter)
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider} from 'react-redux'
 import './index.css';
 import App from './App';
-
+import store from './store/index'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 );
+
